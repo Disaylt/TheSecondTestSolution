f66ba33a68a69ba1b6915221861b8ab710f1b3b3
Disaylt <disofban@gmail.com>
2024-09-26 01:32:10 +0300

    feat: implement controller

--- src/TheSecondTestSolution.Api/Controllers/TopicController.cs
+++ src/TheSecondTestSolution.Api/Controllers/TopicController.cs
@@ -1,5 +1,9 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using MediatR;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using TheSecondTestSolution.Application.Commands;
+using TheSecondTestSolution.Application.Models;
+using TheSecondTestSolution.Application.Queries;
 
 namespace TheSecondTestSolution.Api.Controllers
 {
@@ -7,5 +11,30 @@ namespace TheSecondTestSolution.Api.Controllers
     [ApiController]
     public class TopicController : ControllerBase
     {
+        private readonly IMediator _mediator;
+
+        public TopicController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetAsync(int id)
+        {
+            GetTopicByIdQuery query = new GetTopicByIdQuery { Id = id };
+
+            TopicDto topic = await _mediator.Send(query);
+
+            return Ok(topic);
+        }
+
+        public async Task<IActionResult> AddAsync([FromBody] TopicDto body)
+        {
+            AddTopicCommand command = new AddTopicCommand { Topic = body };
+
+            await _mediator.Send(command);
+
+            return Ok();
+        }
     }
 }
