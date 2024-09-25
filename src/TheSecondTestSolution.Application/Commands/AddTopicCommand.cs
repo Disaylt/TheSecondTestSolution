25abcdfc830157fb08460fe0e8f0ab561381f6cf
Disaylt <disofban@gmail.com>
2024-09-25 23:34:51 +0300

    init: implement command

---
+++ src/TheSecondTestSolution.Application/Commands/AddTopicCommand.cs
@@ -0,0 +1,15 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TheSecondTestSolution.Application.Models;
+
+namespace TheSecondTestSolution.Application.Commands
+{
+    public class AddTopicCommand : ICommand<Unit>
+    {
+        public required TopicDto Topic { get; set; }
+    }
+}
---
+++ src/TheSecondTestSolution.Application/Commands/AddTopicCommandHandler.cs
@@ -0,0 +1,37 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TheSecondTestSolution.Application.Services;
+using TheSecondTestSolution.Domain.Entities;
+using TheSecondTestSolution.Domain.Seed;
+
+namespace TheSecondTestSolution.Application.Commands
+{
+    public class AddTopicCommandHandler : IRequestHandler<AddTopicCommand, Unit>
+    {
+        private readonly IRepository<TopicEntity> _topicRepository;
+        private readonly ITopicService _topicService;
+
+        public AddTopicCommandHandler(IRepository<TopicEntity> topicRepository,
+            ITopicService topicService)
+        {
+            _topicRepository = topicRepository;
+            _topicService = topicService;
+        }
+
+        public async Task<Unit> Handle(AddTopicCommand request, CancellationToken cancellationToken)
+        {
+            TopicEntity entity = _topicService.Create(request.Topic);
+            await _topicRepository.AddAsync(entity, cancellationToken);
+
+            await _topicRepository
+                .UnitOfWork
+                .SaveEntitiesAsync(cancellationToken);
+
+            return Unit.Value;
+        }
+    }
+}
