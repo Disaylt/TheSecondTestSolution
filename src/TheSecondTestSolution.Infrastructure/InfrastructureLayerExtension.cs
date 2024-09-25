202c31b8b3c7e898b98dc7d3086db03834006bfe
Disaylt <disofban@gmail.com>
2024-09-26 00:12:56 +0300

    init: create infrastructure

---
+++ src/TheSecondTestSolution.Infrastructure/InfrastructureLayerExtension.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TheSecondTestSolution.Infrastructure
+{
+    public static class InfrastructureLayerExtension
+    {
+        public static void AddInfrastructureLayer(this IServiceCollection collection, IConfiguration configuration)
+        {
+            IConfigurationSection connectionSection = configuration.GetSection("ConnectionStrings");
+
+            string dbConnection = connectionSection.GetValue<string>("nasaAsteroids") ?? string.Empty;
+            string redisConnection = connectionSection.GetValue<string>("redis") ?? string.Empty;
+
+        }
+    }
+}
