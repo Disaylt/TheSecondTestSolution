5e6846b1dd21443be505e4e436c6ac61fd149acf
Disaylt <disofban@gmail.com>
2024-09-25 23:35:58 +0300

    init: create cache rep

---
+++ src/TheSecondTestSolution.Domain/Seed/ICacheRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TheSecondTestSolution.Domain.Seed
+{
+    public interface ICacheRepository<T> where T : class
+    {
+        Task<T?> GetAsync(string key);
+        Task<IEnumerable<T>?> GetRangeAsync(string key);
+        Task SetAsync(string key, T value, TimeSpan expire);
+        Task SetRangeAsync(string key, IEnumerable<T> value, TimeSpan expire);
+    }
+}
