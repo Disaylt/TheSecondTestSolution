853018ab3994cafd275ba71716c97b5d4b80458a
Disaylt <disofban@gmail.com>
2024-09-25 22:21:16 +0300

    feat: implement entity

--- src/TheSecondTestSolution.Domain/Entities/TopicEntity.cs
+++ src/TheSecondTestSolution.Domain/Entities/TopicEntity.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TheSecondTestSolution.Domain.Events;
 using TheSecondTestSolution.Domain.ValueObjects;
 
 namespace TheSecondTestSolution.Domain.Entities
@@ -34,6 +35,45 @@ namespace TheSecondTestSolution.Domain.Entities
             Title = title;
         }
 
+        public void SetTitle(string title)
+        {
+            string oldValue = Title;
+            Title = title;
+
+            AddEvent(new NewTopicTitleEvent(this, oldValue));
+        }
+
+        public void SetType(string type)
+        {
+            string oldValue = Type;
+            Type = type;
+
+            AddEvent(new NewTopicTypeEvent(this, oldValue));
+        }
+
+        public void SetUserName(string userName)
+        {
+            string oldValue = UserName;
+            UserName = userName;
+
+            AddEvent(new NewTopicUserEvent(this, oldValue));
+        }
+
+        public void SetLink(LinkValueObject link)
+        {
+            LinkValueObject oldValue = Link;
+            Link = link;
+
+            AddEvent(new NewTopicLinkEvent(this, oldValue));
+        }
+
+        public void SetScore(ScoreValueObject score)
+        {
+            ScoreValueObject oldValue = Score;
+            Score = score;
+
+            AddEvent(new NewTopicScoreEvent(this, oldValue));
+        }
 
     }
 }
