dc352b143d3196e03fc4ec4867a952144cfa6a52
Disaylt <disofban@gmail.com>
2024-09-26 01:24:53 +0300

    init: add validator

--- src/TheSecondTestSolution.Application/Models/TopicDto.cs
+++ src/TheSecondTestSolution.Application/Models/TopicDto.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,10 +9,15 @@ namespace TheSecondTestSolution.Application.Models
 {
     public record class TopicDto
     {
+        [MinLength(5, ErrorMessage = "Пользователь должен содержать более 5 символов.")]
         public string By { get; init; } = string.Empty;
+
+        [MinLength(10, ErrorMessage = "Пользователь должен содержать более 10 символов.")]
         public string Title { get; init; } = string.Empty;
         public string Type { get; init; } = string.Empty;
         public string Url { get; init; } = string.Empty;
+
+        [Range(0, int.MaxValue, ErrorMessage = "Неверно введено значение счета.")]
         public int Score { get; init; }
     }
 }
---
+++ src/TheSecondTestSolution.Application/Queries/GetTopicByIdQueryValidator.cs
@@ -0,0 +1,19 @@
+﻿using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TheSecondTestSolution.Application.Queries
+{
+    public class GetTopicByIdQueryValidator : AbstractValidator<GetTopicByIdQuery>
+    {
+        public GetTopicByIdQueryValidator()
+        {
+            RuleFor(v => v.Id)
+                .LessThan(0)
+                .WithMessage("Id is less than 0.");
+        }
+    }
+}
