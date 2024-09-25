bb5c0018a18742dcaec962e53d81868d1c9f6759
Disaylt <disofban@gmail.com>
2024-09-25 21:22:02 +0300

    init: create value object

---
+++ src/TheSecondTestSolution.Domain/ValueObjects/Score.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TheSecondTestSolution.Domain.ValueObjects
+{
+    public class Score : BaseValueObject
+    {
+        public int Value { get; }
+
+        public Score(int value)
+        {
+            if(value < 0)
+            {
+                throw new ArgumentException($"Score cannot be less than 0, value = {value}");
+            }
+
+            Value = value;
+        }
+
+        protected override IEnumerable<object?> GetEqualityComponents()
+        {
+            yield return Value;
+        }
+    }
+}
