77a28c9ec7635092f8669975d56bed36a636f253
Disaylt <disofban@gmail.com>
2024-09-25 21:26:46 +0300

    init: create IEntity

---
+++ src/TheSecondTestSolution.Domain/Seed/IEntity.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TheSecondTestSolution.Domain.Seed
+{
+    public interface IEntity
+    {
+        public int Id { get; }
+        public DateTime CreateDate { get; }
+        public DateTime UpdateDate { get; }
+        public void RefreshUpdateDate();
+        public IReadOnlyCollection<INotification> Events { get; }
+        public void AddEvent(INotification notification);
+        public void RemoveEvent(INotification notification);
+        public void ClearEvents();
+    }
+}
