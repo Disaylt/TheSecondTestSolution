6004270b6d345cdcd4c93b36dfce1d798b252c54
Disaylt <disofban@gmail.com>
2024-09-25 21:53:25 +0300

    feat: add constructor and properties

--- src/TheSecondTestSolution.Domain/Entities/TopicEntity.cs
+++ src/TheSecondTestSolution.Domain/Entities/TopicEntity.cs
@@ -11,8 +11,26 @@ namespace TheSecondTestSolution.Domain.Entities
     {
         public int Id { get; private set; }
         public ScoreValueObject Score { get; private set; }
+        public LinkValueObject Link { get; private set; }
         public string UserName { get; private set; }
         public string Type { get; private set; }
-        public string Url
+
+        protected TopicEntity()
+        {
+
+        }
+
+        public TopicEntity(ScoreValueObject score,
+            LinkValueObject link,
+            string userName,
+            string type)
+        {
+            Score = score;
+            Link = link;
+            UserName = userName;
+            Type = type;
+        }
+
+
     }
 }
