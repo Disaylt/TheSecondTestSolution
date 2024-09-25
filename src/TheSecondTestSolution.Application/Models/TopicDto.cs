04d78c243c2bc1f952df937a14f89b2606e7e936
Disaylt <disofban@gmail.com>
2024-09-25 22:54:33 +0300

    init: create topic dto

---
+++ src/TheSecondTestSolution.Application/Models/TopicDto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TheSecondTestSolution.Application.Models
+{
+    public record class TopicDto
+    {
+        public string By { get; init; } = string.Empty;
+        public string Title { get; init; } = string.Empty;
+        public string Type { get; init; } = string.Empty;
+        public string Url { get; init; } = string.Empty;
+        public int Score { get; init; }
+    }
+}
