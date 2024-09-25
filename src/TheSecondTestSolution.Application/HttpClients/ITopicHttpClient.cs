c451b099a91c8a1b0c0cc0a9891f446eeb3ed18d
Disaylt <disofban@gmail.com>
2024-09-25 22:40:19 +0300

    init: create topic web model

--- src/TheSecondTestSolution.Application/HttpClients/ITopicHttpClient.cs
+++ src/TheSecondTestSolution.Application/HttpClients/ITopicHttpClient.cs
@@ -3,12 +3,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TheSecondTestSolution.Application.Models;
 
 namespace TheSecondTestSolution.Application.HttpClients
 {
     public interface ITopicHttpClient
     {
         Task<IReadOnlyCollection<int>> GetIdsAsync();
-        Task GetByIdAsync(int id);
+        Task<TopicWebModel> GetByIdAsync(int id);
     }
 }
---
+++ src/TheSecondTestSolution.Application/Models/TopicWebModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TheSecondTestSolution.Application.Models
+{
+    public class TopicWebModel
+    {
+        public required string By { get; set; }
+        public required string Title { get; set; }
+        public required string Type { get; set; }
+        public required string Url { get; set; }
+        public int Score { get; set; }
+
+    }
+}
