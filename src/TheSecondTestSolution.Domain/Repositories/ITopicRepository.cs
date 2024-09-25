9f307035f4beea9c4dc13a2f970ce2eb321142a2
Disaylt <disofban@gmail.com>
2024-09-25 23:44:53 +0300

    init: create topic repository

---
+++ src/TheSecondTestSolution.Domain/Repositories/ITopicRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TheSecondTestSolution.Domain.Entities;
+using TheSecondTestSolution.Domain.Seed;
+
+namespace TheSecondTestSolution.Domain.Repositories
+{
+    public interface ITopicRepository : IRepository<TopicEntity>
+    {
+        Task<TopicEntity> FindByIdRequiredAsync(int id);
+        Task<IEnumerable<TopicEntity>> GetAllAsync();
+    }
+}
