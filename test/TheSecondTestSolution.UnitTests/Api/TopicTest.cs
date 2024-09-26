69631ac178622f0a952a4d76b9efda0e35ccc7b3
Disaylt <disofban@gmail.com>
2024-09-26 03:06:25 +0300

    init: implement topic tests

---
+++ test/TheSecondTestSolution.UnitTests/Api/TopicTest.cs
@@ -0,0 +1,70 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using TheSecondTestSolution.Api.Controllers;
+using TheSecondTestSolution.Application.Models;
+using TheSecondTestSolution.Domain.Seed;
+using TheSecondTestSolution.UnitTests.Mocks.MediatR;
+
+namespace TheSecondTestSolution.UnitTests.Api
+{
+    public class TopicTest
+    {
+        private TopicController _topicController;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _topicController = new TopicController(MediatrMockFactory.ForTopicController());
+        }
+
+        [TestCase(0)]
+        public async Task GetTopic_InputId_ExpectedTopic(int id)
+        {
+            IActionResult actionResult = await _topicController.GetAsync(id);
+            OkObjectResult okResult = actionResult as OkObjectResult
+                ?? throw new NullReferenceException();
+
+            Assert.That(okResult.StatusCode, Is.EqualTo(200));
+        }
+
+        [TestCase(1)]
+        public void GetTopic_InputId_ExpectedNotFound(int id)
+        {
+            RootExeption exeption = Assert.ThrowsAsync<RootExeption>(() => _topicController.GetAsync(id));
+            Assert.That(exeption.StatusCode, Is.EqualTo(HttpStatusCode.NotFound));
+        }
+
+        [TestCase(-1)]
+        public void GetTopic_InputId_ExpectedBadRequest(int id)
+        {
+            RootExeption exeption = Assert.ThrowsAsync<RootExeption>(() => _topicController.GetAsync(id));
+            Assert.That(exeption.StatusCode, Is.EqualTo(HttpStatusCode.BadRequest));
+        }
+
+        //По подобному принципу проверяем каждое значение в топике, которые могут опракинуть валидация, для примера взял 2
+        [TestCase("1234")]
+        public void CreateTopic_InputTitle_ExpectedBadRequest(string title)
+        {
+            TopicDto topic = new TopicDto { Title = title };
+            RootExeption exeption = Assert.ThrowsAsync<RootExeption>(() => _topicController.AddAsync(topic));
+            Assert.That(exeption.StatusCode, Is.EqualTo(HttpStatusCode.BadRequest));
+        }
+
+        [TestCase("12345678901")]
+        public async Task CreateTopic_InputTitle_ExpectedOk(string title)
+        {
+            TopicDto topic = new TopicDto { Title = title };
+            IActionResult actionResult = await _topicController.AddAsync(topic);
+            OkResult okResult = actionResult as OkResult
+                ?? throw new NullReferenceException();
+
+            Assert.That(okResult.StatusCode, Is.EqualTo(200));
+        }
+    }
+}
---
+++ test/TheSecondTestSolution.UnitTests/Mocks/MediatR/MediatrMockFactory.cs
@@ -0,0 +1,37 @@
+﻿using MediatR;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TheSecondTestSolution.Application.Commands;
+using TheSecondTestSolution.Application.Models;
+using TheSecondTestSolution.Application.Queries;
+using TheSecondTestSolution.Domain.Seed;
+
+namespace TheSecondTestSolution.UnitTests.Mocks.MediatR
+{
+    internal class MediatrMockFactory
+    {
+        public static IMediator ForTopicController()
+        {
+            Mock<IMediator> mock = new Mock<IMediator>();
+
+            mock.Setup(x => x.Send(It.Is<GetTopicByIdQuery>(l => l.Id == 0), It.IsAny<CancellationToken>()))
+                .ReturnsAsync(new TopicDto());
+            mock.Setup(x => x.Send(It.Is<GetTopicByIdQuery>(l => l.Id == 1), It.IsAny<CancellationToken>()))
+               .Throws(() => new RootExeption(System.Net.HttpStatusCode.NotFound, string.Empty));
+            mock.Setup(x => x.Send(It.Is<GetTopicByIdQuery>(l => l.Id < 0), It.IsAny<CancellationToken>()))
+                .Throws(() => new RootExeption(System.Net.HttpStatusCode.BadRequest, string.Empty));
+
+            mock.Setup(x => x.Send(It.Is<AddTopicCommand>(l => l.Topic.Title.Length < 10), It.IsAny<CancellationToken>()))
+                .Throws(() => new RootExeption(System.Net.HttpStatusCode.BadRequest, string.Empty));
+
+            mock.Setup(x => x.Send(It.Is<AddTopicCommand>(l => l.Topic.Title.Length >= 10), It.IsAny<CancellationToken>()))
+                .ReturnsAsync(Unit.Value);
+
+            return mock.Object;
+        }
+    }
+}
